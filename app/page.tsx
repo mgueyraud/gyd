5ed67b1ce4bc036a74cdb258f58a7167d74edeca
mgueyraud <mgueyraud.junior@gmail.com>
2023-12-30 15:43:23 -0300

    Home page tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Mario</h1>
+      <h1 className="text-lg font-bold">Mario</h1>
     </main>
   );
 }
